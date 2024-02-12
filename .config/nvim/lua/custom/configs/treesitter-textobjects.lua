d65483c7604c59c3be039284697d16037ee01b06
Jose Cardona <jose@josecardona.dev>
2024-02-12 18:59:28 +0100

    nvim config

---
+++ .config/nvim/lua/custom/configs/treesitter-textobjects.lua
@@ -0,0 +1,37 @@
+require("nvim-treesitter.configs").setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+      },
+      selection_modes = {
+        ["@parameter.outer"] = "v", -- charwise
+        ["@function.outer"] = "V", -- linewise
+        ["@class.outer"] = "<c-v>", -- blockwise
+      },
+      include_surrounding_whitespace = true,
+    },
+
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>>"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader><"] = "@parameter.inner",
+      },
+    },
+
+
+  },
+}
